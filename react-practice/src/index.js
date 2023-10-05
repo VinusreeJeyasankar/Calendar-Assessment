001b0ccb45c37c717b609fbee369b461b30dd4e3
vinusreejeyasankar <vinusreejeyasankar@gmail.com>
2023-10-05 13:35:39 +0530

    toastContainer added

--- react-practice/src/index.js
+++ react-practice/src/index.js
@@ -5,12 +5,15 @@ import store from "./store/store";
 import { Provider } from "react-redux";
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
     <App />
+    <ToastContainer />
     </Provider>
   </React.StrictMode>
 );
